12b4138a959fe8f2e04537cf3c47b37939d79515
Rk Reja <Rk Reja@VM-WIN10.localdomain>
2017-07-29 22:31:37 -0400

    check git

--- src/oop/PhoneTest.java
+++ src/oop/PhoneTest.java
@@ -4,9 +4,7 @@ public class PhoneTest {
 
 	public static void main(String[] args) {
 		
-		Phone myphone= new Phone();
-		
-		
+		Phone myphone= new Phone();		
 //		myphone.call();
 		myphone.text();
 		//myphone.call(11255254);
