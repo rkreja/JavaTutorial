2bab4667a03c7e8513509175711226ed5fb47bcd
Rk Reja <Rk Reja@VM-WIN10.localdomain>
2017-10-01 00:36:01 -0400

    after polymorphism

--- src/com/timeandtraining/phone/Zmobile.java
+++ src/com/timeandtraining/phone/Zmobile.java
@@ -14,6 +14,14 @@ public class Zmobile {
 		return sp;
 		
 	}
+	public SmartPhone purchaseNonSmartPhone(Human human, String model) {
+		System.out.println("Thank you "+ human.getName()+", for shopping with ZMobile.\nEnjoy your "+ model);
+		
+		SmartPhone sp=new SmartPhone(model, true, true);
+		human.setPhone(sp);
+		return sp;
+		
+	}
 	
 	
 
--- src/com/timeandtraining/testprogram/App.java
+++ src/com/timeandtraining/testprogram/App.java
@@ -1,10 +1,6 @@
 package com.timeandtraining.testprogram;
 
-import com.timeandtraining.humans.Doctor;
 import com.timeandtraining.humans.Human;
-import com.timeandtraining.humans.Singer;
-import com.timeandtraining.humans.Teacher;
-import com.timeandtraining.phone.SmartPhone;
 import com.timeandtraining.phone.Zmobile;
 
 public class App {
@@ -13,22 +9,9 @@ public class App {
 		
 		
 		Zmobile zmobile = new Zmobile();
-		
-		
-		Human shariar = new Human("Shariar");
-		Human azad = new Human("Azad");
-		Human topu = new Human("Topu");
-		
-		SmartPhone sp;
-		sp=zmobile.purchaseSmartPhone(shariar,"galaxy s8");
-		sp=zmobile.purchaseSmartPhone(topu,"iphone 7");
-		sp=zmobile.purchaseSmartPhone(azad,"galaxy s5");
-		
-		
-		shariar.getPhone().call(9898);
-		
-		topu.getPhone().takePhoto();
-		azad.getPhone().settings();
+		Human monica = new Human("Monica");
+		zmobile.purchaseSmartPhone(monica, "Nokia 3310");
+		monica.getPhone().takePhoto();
 	
 		
 		
